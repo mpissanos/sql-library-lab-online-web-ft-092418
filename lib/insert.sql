3d2845bbadedc8d173aa2b322438f902037b7970
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:45:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,7 +23,7 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
   (" ", "Let's eat!", "hobbit", 1, 1),
   (" ", "The fate of the world is upon us", "human", 1, 1),
   (" " , "There's something in my eye", "elf", 1, 1),
-  (" ", "Mathmatics rules", "human", 2, 2),
+  ("Roland Deschain ", "Mathmatics rules", "human", 2, 2),
   (" ", "Seldon rules", "human", 2, 2),
   (" ", "", "human", 2, 2),
   (" ", "Stay the course", "human", 2, 2);
