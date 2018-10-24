b9f61b3a40058a8a0c869a189619f673dc06c535
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:39:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,41 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("Dark Tower", 2, 2);
 
 INSERT INTO subgenres (name) VALUES
-("Fantasy"),
+  ("Fantasy"),
 ("Horror");
 
-INSERT INTO books
+INSERT INTO books (title, year, series_id) VALUES
+  ("Fellowship of the Ring", 1954, 1),
+  ("The Two Towers", 1954, 1),
+  ("The Return of the King", 1955, 1),
+  ("Foundation", 1951, 2),
+  ("Foundation and Empire", 1952, 2),
+  ("Second Foundation", 1953, 2);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+  ("Bilbo Baggins", "Never give up", "hobbit", 1, 1),
+  ("Pippin Took", "Let's eat!", "hobbit", 1, 1),
+  ("Gandalf", "The fate of the world is upon us", "human", 1, 1),
+  ("Legolas", "There's something in my eye", "elf", 1, 1),
+  ("Harry Seldon", "Mathmatics rules", "human", 2, 2),
+  ("Gaal Dornick", "Seldon rules", "human", 2, 2),
+  ("King Leopold", "", "human", 2, 2),
+  ("Salvor Hardin", "Stay the course", "human", 2, 2);
+
+INSERT INTO character_books (character_id, book_id) VALUES
+  (1,1),
+  (1,2),
+  (1,3),
+  (2,1),
+  (2,2),
+  (2,3),
+  (3,1),
+  (4,2),
+  (5,4),
+  (5,5),
+  (5,6),
+  (6,4),
+  (6,5),
+  (6,6),
+  (7,4),
+  (8,5);
