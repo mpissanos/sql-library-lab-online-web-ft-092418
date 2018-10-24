6ea7872508e7c38d33efc2bdb007561bfcbc9264
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:37:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,5 +3,7 @@ INSERT INTO authors (name) VALUES
 ("Stephen King");
 
 INSERT INTO series (title, author_id, subgenre_id) VALUES
-()
-()
+("Harry Potter", 1, 2)
+("Dark Tower", 2, 2)
+
+INSERT INTO subgenres ()
