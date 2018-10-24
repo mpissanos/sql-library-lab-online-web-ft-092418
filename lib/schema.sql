24f7aa75001142f13dfd093d4bbf67b047b08073
Manny Pissanos <mpissano@gmail.com>
2018-10-24 17:28:14 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,27 +1,27 @@
-CREATE TABLE SERIES (
+CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   sub_genre_id INTEGER
 );
 
-CREATE TABLE SUB-GENRE(
+CREATE TABLE sub_genre(
   id INTEGER PRIMARY KEY,
   name TEXT
   );
 
-CREATE TABLE AUTHORS (
+CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT,
 );
 
-CREATE TABLE BOOKS (
+CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   series_id INTEGER
 );
 
-CREATE TABLE CHARACTERS (
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
@@ -30,4 +30,8 @@ CREATE TABLE CHARACTERS (
   series_id INTEGER
 );
 
-CREATE
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
