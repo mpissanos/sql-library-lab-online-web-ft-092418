67108c8b57327f23e478bc7921af80e065761397
Manny Pissanos <mpissano@gmail.com>
2018-10-24 18:05:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year 
-   FROM series JOIN books ON series.id = books
+   FROM series JOIN books ON series.id = books.series_id
+   
 end
 
 def select_name_and_motto_of_char_with_longest_motto
